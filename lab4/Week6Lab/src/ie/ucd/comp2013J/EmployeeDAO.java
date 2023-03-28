241a1e52d2ce9b4b0d360a0cb6c5d562306dd89d
KelvinQiu <kelvinqiu802@outlook.com>
2023-03-28 17:14:43 +0800

    feat: lab4 v1

--- lab4/Week6Lab/src/ie/ucd/comp2013J/EmployeeDAO.java
+++ lab4/Week6Lab/src/ie/ucd/comp2013J/EmployeeDAO.java
@@ -1,61 +1,174 @@
 package ie.ucd.comp2013J;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeDAO {
     public static List<Employee> getAllEmployees() {
         List<Employee> employees = new ArrayList<Employee>();
-
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
         try {
-            Connection conn = JDBCTool.getConnection();
-            Statement st = conn.createStatement();
-
-            ResultSet rs = st.executeQuery("SELECT * FROM employee");
-
+            conn = JDBCTool.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("SELECT * FROM employee");
             while (rs.next()) {
-                int empno = rs.getInt("empno");
-                String firstname = rs.getString("firstname");
-                String familyname = rs.getString("familyname");
-                String job = rs.getString("job");
-                float salary = rs.getFloat("salary");
-                int deptno = rs.getInt("deptno");
-
-                Employee e = new Employee(empno, firstname, familyname, job, salary, deptno);
-
-                employees.add(e);
+                employees.add(createEmployeeWithResultSet(rs));
             }
-
-            rs.close();
-            st.close();
-            conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                JDBCTool.closeAllConnections(conn, st, rs);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
         return employees;
     }
 
     //TODO Question 2
     public static Employee getEmployeeByID(int eid) {
+        Connection connect = null;
+        PreparedStatement st = null;
+        ResultSet result = null;
+        try {
+            connect = JDBCTool.getConnection();
+            st = connect.prepareStatement("SELECT * FROM employee WHERE empno = ?;");
+            st.setInt(1, eid);
+            result = st.executeQuery();
+            if (result.next()) {
+                return createEmployeeWithResultSet(result);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                JDBCTool.closeAllConnections(connect, st, result);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         return null;
+    }
+
+    private static Employee createEmployeeWithResultSet(ResultSet result) throws SQLException {
+        int empno = result.getInt("empno");
+        String firstname = result.getString("firstname");
+        String lastname = result.getString("familyname");
+        String job = result.getString("job");
+        float salary = result.getFloat("salary");
+        int deptno = result.getInt("deptno");
+        return new Employee(empno, firstname, lastname, job
+                , salary, deptno);
     }
 
     //TODO Question 3
     public static boolean deleteEmployeeByID(int eid) {
+        Connection connect = null;
+        PreparedStatement st = null;
+        ResultSet result = null;
+        try {
+            connect = JDBCTool.getConnection();
+            st = connect.prepareStatement("DELETE FROM employee WHERE empno = ?");
+            st.setInt(1, eid);
+            return st.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                JDBCTool.closeAllConnections(connect, st, result);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         return false;
     }
 
     //TODO Question 4
     public static boolean updateEmployee(Employee e) {
+        Connection connect = null;
+        PreparedStatement st = null;
+        ResultSet result = null;
+
+        int eid = e.getEmpno();
+        String firstname = e.getFirstname();
+        String familyname = e.getFamilyname();
+        String job = e.getJob();
+        float salary = e.getSalary();
+        int deptno = e.getDeptno();
+
+        try {
+            connect = JDBCTool.getConnection();
+            st = connect.prepareStatement("SELECT * FROM employee WHERE empno = ?");
+            st.setInt(1, eid);
+            result = st.executeQuery();
+            if (result.next() && employeeDataChanged(result, firstname, familyname, job, salary, deptno)) {
+                st = connect.prepareStatement("UPDATE employee SET firstname = ?, " +
+                        "familyname = ?, job = ?, salary = ?, deptno = ? WHERE empno = ?;");
+                st.setString(1, firstname);
+                st.setString(2, familyname);
+                st.setString(3, job);
+                st.setFloat(4, salary);
+                st.setInt(5, deptno);
+                st.setInt(6, eid);
+                return st.execute();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                JDBCTool.closeAllConnections(connect, st, result);
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
         return false;
+    }
+
+    private static boolean employeeDataChanged(ResultSet result, String firstname, String familyname, String job, float salary, int deptno) throws SQLException {
+        String firstnameDB = result.getString("firstname");
+        String familynameDB = result.getString("familyname");
+        String jobDB = result.getString("job");
+        float salaryDB = result.getFloat("salary");
+        int deptnoDB = result.getInt("deptno");
+        return !firstname.equals(firstnameDB) || !familyname.equals(familynameDB) || !job.equals(jobDB) || salary != salaryDB || deptno != deptnoDB;
     }
 
     //TODO Question 5
     public static boolean insertEmployee(Employee e) {
+        int eid = e.getEmpno();
+        String firstname = e.getFirstname();
+        String familyname = e.getFamilyname();
+        String job = e.getJob();
+        float salary = e.getSalary();
+        int deptno = e.getDeptno();
+
+        Connection connect = null;
+        PreparedStatement st = null;
+        ResultSet result = null;
+
+        try {
+            connect = JDBCTool.getConnection();
+            st = connect.prepareStatement("INSERT INTO employee VALUES (?, ?, ?, ?, ?, ?);");
+            st.setInt(1, eid);
+            st.setString(2, firstname);
+            st.setString(3, familyname);
+            st.setString(4, job);
+            st.setFloat(5, salary);
+            st.setInt(6, deptno);
+            return st.execute();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                JDBCTool.closeAllConnections(connect, st, result);
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
         return false;
     }
 }
--- lab4/Week6Lab/src/ie/ucd/comp2013J/JDBCTool.java
+++ lab4/Week6Lab/src/ie/ucd/comp2013J/JDBCTool.java
@@ -1,25 +1,29 @@
 package ie.ucd.comp2013J;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JDBCTool {
-	public static Connection getConnection(String url, String dbname, String username, String password) {
-		Connection conn = null;
-		try {
-			conn = DriverManager.getConnection("jdbc:mysql://" + url + "/" +
-					dbname + "?" + "user=" +
-					username + "&password=" + password);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return conn;
-	}
+    public static Connection getConnection(String url, String dbname, String username, String password) throws SQLException {
+        Connection conn = null;
+        conn = DriverManager.getConnection("jdbc:mysql://" + url + "/" +
+                dbname + "?" + "user=" +
+                username + "&password=" + password);
+        return conn;
+    }
 
-	//TODO Question 1 Check user name and password
-	public static Connection getConnection() {
-		return JDBCTool.getConnection("localhost", "employee", "root", "123456");
-	}
+    public static Connection getConnection() throws SQLException {
+        return JDBCTool.getConnection("localhost:3306", "employee", "root", "");
+    }
 
+    public static void closeAllConnections(Connection connect, Statement st, ResultSet result) throws SQLException {
+        if (connect != null) {
+            connect.close();
+        }
+        if (st != null) {
+            st.close();
+        }
+        if (result != null) {
+            result.close();
+        }
+    }
 }
