04ffa502b47168160565e827d9e607e261ff120b
KelvinQiu <kelvinqiu802@outlook.com>
2023-03-28 15:17:09 +0800

    style: code style

--- lab4/Week6Lab/src/ie/ucd/comp2013J/Test.java
+++ lab4/Week6Lab/src/ie/ucd/comp2013J/Test.java
@@ -4,22 +4,17 @@ import java.util.List;
 
 public class Test {
 
-	public static void main(String[] args) {
-		
-		
-		//Test getAllEmployees()
-		List<Employee> employees = EmployeeDAO.getAllEmployees();
-		
-		for(int i=0; i<employees.size(); i++) {
-			Employee employee = employees.get(i);
-			
-			System.out.println(employee.toString()+"\n");
-		}
-		
-		//TODO Question 6 
-		
-		
+    public static void main(String[] args) {
+        //Test getAllEmployees()
+        List<Employee> employees = EmployeeDAO.getAllEmployees();
 
-	}
+        for (int i = 0; i < employees.size(); i++) {
+            Employee employee = employees.get(i);
+
+            System.out.println(employee.toString() + "\n");
+        }
+
+        //TODO Question 6
+    }
 
 }
